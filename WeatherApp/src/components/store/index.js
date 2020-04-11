55b0438b9fa651fac974323e4b48f43543b648e0
AlexSuboch <53902207+AlexSuboch@users.noreply.github.com>
2020-04-11 15:16:35 +0300

    initial

--- WeatherApp/src/components/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux'
-import { dataReducer } from './reducer'
-import { rootMiddleware } from './rootMiddleware'
-
-export const store = createStore(dataReducer, rootMiddleware)
--- WeatherApp/src/components/store/rootMiddleware.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware } from 'redux'
-import { usersMiddleware } from './middleware'
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-
-export const rootMiddleware = composeWithDevTools(applyMiddleware(usersMiddleware))
